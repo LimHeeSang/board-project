7213f9d1d30fcc036203fbf85e1d7c3f8eada6af
heesang <dlagmltkd12@naver.com>
2023-01-15 21:37:58 +0900

    #17 - 회원 도메인 개발
    'UserAccount' 이름으로 회원 계정 도메인 개발
    'user', 'account' 는 mysql 예약어이므로 피함
    
    Repository 인터페이스까지 추가 개발

---
+++ src/main/java/com/heesang/boardproject/domain/UserAccount.java
@@ -0,0 +1,70 @@
+package com.heesang.boardproject.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@ToString
+@Getter
+@Table(indexes = {
+        @Index(columnList = "userId"),
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class UserAccount extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String userId;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 100)
+    private String nickname;
+
+    private String memo;
+
+    protected UserAccount() {
+    }
+
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.memo = memo;
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
+        return new UserAccount(userId, userPassword, email, nickname, memo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAccount that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
